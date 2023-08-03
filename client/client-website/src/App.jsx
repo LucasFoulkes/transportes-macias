ff65700a0536e91888afb89493d1fc8846903bb4
LucasFoulkes <lucasfoulkes@gmail.com>
2023-08-03 03:18:40 -0500

    web client website almost done

--- client/client-website/src/App.jsx
+++ client/client-website/src/App.jsx
@@ -1,137 +1,128 @@
 import "./App.scss";
 import Form from "./components/Form/Form";
+import truckImage from "./assets/9.png";
+import Navigation from "./components/Navigation";
+
+const HeroSection = () => (
+  <section id="hero">
+    <h1>Translogistica Macias</h1>
+    <span
+      onClick={() =>
+        document
+          .getElementById("calculadora")
+          .scrollIntoView({ behavior: "smooth" })
+      }
+    >
+      has una reserva hoy!
+    </span>
+  </section>
+);
+
+const ContactSection = () => (
+  <section id="contacto">
+    <h1>
+      Bienvenido a Translogisticas Macias, su solución completa y segura en
+      logística. Nos distinguimos por nuestra eficiencia, rapidez y compromiso
+      con el cliente.
+    </h1>
+    <ul>
+      <li>
+        <a href="https://www.facebook.com/p/Translogistica-Mac%C3%ADas-100070950350531">
+          Facebook
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/mudanzaycargamacias/?hl=en">
+          Instagram
+        </a>
+      </li>
+      <li>
+        <a href="">Email</a>
+      </li>
+      <li>
+        <a href="">WhatsApp</a>
+      </li>
+    </ul>
+  </section>
+);
+
+const ServicesSection = () => (
+  <section id="servicios">
+    <ul>
+      <h1>Servicios</h1>
+      <li>
+        <span>
+          <strong>&gt;</strong>
+        </span>{" "}
+        Rastreo en Tiempo Real
+      </li>
+      <li>
+        <span>
+          <strong>&gt;</strong>
+        </span>{" "}
+        Empaque Seguro y Eficiente
+      </li>
+      <li>
+        <span>
+          <strong>&gt;</strong>
+        </span>{" "}
+        Asistencia en Desempaque: Desembalaje Cuidadoso y Organizado
+      </li>
+      <li>
+        <span>
+          <strong>&gt;</strong>
+        </span>{" "}
+        Mudanzas sin Estrés: Relocación de sus Pertenencias
+      </li>
+    </ul>
+    <img src={truckImage} alt="Camión en un mapa" />
+  </section>
+);
+
+const CalculatorSection = () => (
+  <section id="calculadora">
+    <h1>Calculadora </h1>
+    <p>
+      Utilice nuestra calculadora para obtener una estimación rápida del costo
+      de su mudanza o transporte.
+    </p>
+    <Form />
+  </section>
+);
+
+const CareersSection = () => (
+  <section id="carreras">
+    <h1>Trabaja con Nosotros</h1>
+    <p>
+      Trabajar con nosotros abre oportunidades para acceder a una gran cantidad
+      de clientes y ser parte de una comunidad en crecimiento.
+    </p>
+    <p>ofrecemos</p>
+    <ul>
+      <li>Acceso a una gran cantidad de clientes</li>
+      <li>Estructura de tarifas transparente</li>
+      <li>Oportunidades de crecimiento y desarrollo</li>
+    </ul>
+    <p>
+      ¡Únete a nuestro equipo hoy y sé parte de un servicio de transporte líder
+      en la industria en Guayaquil, Ecuador!
+    </p>
+    <a href="careers.html">Contacto</a>
+  </section>
+);
 
 export default function App() {
   return (
     <>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="#servicios" onClick={(e) => e.preventDefault()}>
-                <button
-                  onClick={() =>
-                    document
-                      .getElementById("servicios")
-                      .scrollIntoView({ behavior: "smooth" })
-                  }
-                >
-                  Servicios
-                </button>
-              </a>
-            </li>
-            <li>
-              <a href="#acerca-de" onClick={(e) => e.preventDefault()}>
-                <button
-                  onClick={() =>
-                    document
-                      .getElementById("acerca-de")
-                      .scrollIntoView({ behavior: "smooth" })
-                  }
-                >
-                  Acerca de Nosotros
-                </button>
-              </a>
-            </li>
-            <li>
-              <a href="#contacto" onClick={(e) => e.preventDefault()}>
-                <button
-                  onClick={() =>
-                    document
-                      .getElementById("contacto")
-                      .scrollIntoView({ behavior: "smooth" })
-                  }
-                >
-                  Contacto
-                </button>
-              </a>
-            </li>
-            <li>
-              <a href="#carreras" onClick={(e) => e.preventDefault()}>
-                <button
-                  onClick={() =>
-                    document
-                      .getElementById("carreras")
-                      .scrollIntoView({ behavior: "smooth" })
-                  }
-                >
-                  Trabaja con Nosotros
-                </button>
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <Navigation />
+        <HeroSection />
       </header>
       <main>
-        <section id="acerca-de">
-          <h1>Acerca de Nosotros</h1>
-          <p>
-            El transporte seguro no es un lujo; es una necesidad. En Transporte
-            Seguro Guayaquil, garantizamos que todos nuestros conductores tienen
-            la documentación adecuada y no tienen antecedentes penales. Esta
-            dedicación a la seguridad asegura que sus bienes sean manejados con
-            la máxima cuidado e integridad.
-          </p>
-        </section>
-        <section id="servicios">
-          <h1>Nuestros Servicios</h1>
-          <p>
-            Nuestro procedimiento de trabajo es sencillo pero eficaz. Usted
-            elige los lugares de recogida y entrega, y paga directamente al
-            camionero. Incluso puede rastrear cuándo llegará el camionero. Con
-            nosotros, tiene el control total de su envío y puede estar seguro de
-            que sus bienes están en buenas manos.
-          </p>
-          <ul>
-            <li>Transporte de Mercancías</li>
-            <li>Mudanzas Residenciales y Comerciales</li>
-            <li>Alquiler de Camiones</li>
-            <li>Servicios Personalizados</li>
-          </ul>
-        </section>
-        <section id="calculadora">
-          <h1>Calculadora de Costos</h1>
-          <p>
-            Utilice nuestra calculadora para obtener una estimación rápida del
-            costo de su mudanza o transporte.
-          </p>
-          <Form />
-        </section>
-        <section id="contacto">
-          <h1>Contacto</h1>
-          <p>
-            Póngase en contacto con nosotros para más información o para
-            programar su próximo servicio en Guayaquil, Ecuador.
-          </p>
-          <address>
-            Correo: info@transporteseguroguayaquil.com
-            <br />
-            Teléfono: 098 089 7073
-          </address>
-        </section>
-        <section id="carreras">
-          <h1>Trabaja con Nosotros</h1>
-          <p>
-            Trabajar con nosotros abre oportunidades para acceder a una gran
-            cantidad de clientes y ser parte de una comunidad en crecimiento. Al
-            trabajar con nosotros, solo paga un 3% del viaje por adelantado y
-            recibe crédito para usar o mejorar su servicio. Nuestro modelo
-            garantiza una compensación justa y apoya su crecimiento en la
-            industria.
-          </p>
-          <p>He aquí por qué deberías elegirnos:</p>
-          <ul>
-            <li>Acceso a una gran cantidad de clientes</li>
-            <li>Estructura de tarifas transparente</li>
-            <li>Oportunidades de crecimiento y desarrollo</li>
-            <li>Ambiente de trabajo solidario</li>
-          </ul>
-          <p>
-            ¡Únete a nuestro equipo hoy y sé parte de un servicio de transporte
-            líder en la industria en Guayaquil, Ecuador!
-          </p>
-          <a href="careers.html">Ver Oportunidades</a>
-        </section>
+        <ContactSection />
+        <ServicesSection />
+        <CalculatorSection />
+        <CareersSection />
       </main>
       <footer>
         <p>&copy; 2023 Transporte Seguro Alquiler de Camiones Guayaquil</p>
